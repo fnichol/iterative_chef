6f1c74cc1c9924aae4989ff482f203c4e4315d48
Fletcher Nichol <fnichol@nichol.ca>
2011-04-17 22:48:06 -0600

    Rocco-ize motd.rb.

--- iterations/initial-draft/cookbooks/sandbox/recipes/motd.rb
+++ iterations/initial-draft/cookbooks/sandbox/recipes/motd.rb
@@ -6,10 +6,29 @@
 # attribute. In the real world, most people would set up a **template**
 # and manage the file's contents that way (think Ruby on Rails view
 # template and you're most of the way there).
+#
+# For more details about how this resource works, consult the Chef
+# [resources][re] wiki page.
+#
+# [re]: http://wiki.opscode.com/display/chef/Resources#Resources-File
 file "/etc/motd.tail" do
+
+  # This will be the owner of the file. We're being a bit explicit here.
   owner   "root"
+
+  # This will be the group ownership of the file. Woah.
   group   "root"
+
+  # This is the default action, to create the file. Other possible
+  # actions are `:delete` to ensure the file is deleted and `:touch`
+  # to update the file's mtime and atime on each run.
   action  :create
+
+  # The contents of our file. The `node[:xxx]` parts will use the
+  # dynamic system values determined by Chef at runtime (using the
+  # [Ohai][oh] gem).
+  #
+  # [oh]: http://wiki.opscode.com/display/chef/Ohai
   content <<-MOTD.gsub(/^ {4}/, '')
 
     ============================================================
