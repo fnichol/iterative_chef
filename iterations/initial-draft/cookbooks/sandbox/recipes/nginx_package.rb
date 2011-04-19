15a69688f8147bd4414acd63240aa7d3289803da
Fletcher Nichol <fnichol@nichol.ca>
2011-04-19 14:58:30 -0600

    Add docs to nginx_package.rb.

--- iterations/initial-draft/cookbooks/sandbox/recipes/nginx_package.rb
+++ iterations/initial-draft/cookbooks/sandbox/recipes/nginx_package.rb
@@ -1,1 +1,4 @@
+# This recipe show you just how easy Chef can get in a simple case.
+# It might look like a waste of space, but when building a more complex
+# system, very modular cookbooks and recipes are a massive win.
 package "nginx"
