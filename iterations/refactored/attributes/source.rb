b2e337ffad2ef8ac0101fc4c26ac38c45ae4a1f3
Fletcher Nichol <fnichol@nichol.ca>
2011-04-19 15:13:59 -0600

    Add basic refactored recipe.

---
+++ iterations/refactored/attributes/source.rb
@@ -0,0 +1,19 @@
+default[:nginx][:version]      = "0.9.7"
+default[:nginx][:install_path] = "/opt/nginx-#{nginx[:version]}"
+default[:nginx][:src_binary]   = "#{nginx[:install_path]}/sbin/nginx"
+
+default[:nginx][:archive_cache] = "/var/cache/downloads/nginx"
+
+default[:nginx][:tar_url] = "http://sysoev.ru/nginx/nginx-#{node[:nginx][:version]}.tar.gz"
+default[:nginx][:tar_checksum] = "2feb0acee473cc360a620ee862907b9570a4121956c40cbd27da35f5b0a96045"
+
+default[:nginx][:packages] = %w{ build-essential binutils-doc autoconf flex bison
+                                 libpcre3 libpcre3-dev libssl-dev }
+
+default[:nginx][:configure_flags] = [
+  "--prefix=#{nginx[:install_path]}",
+  "--conf-path=#{nginx[:dir]}/nginx.conf",
+  "--with-http_ssl_module",
+  "--with-http_gzip_static_module"
+]
+default[:nginx][:extra_configure_flags] = []
--- iterations/refactored/cookbooks/nginx/recipes/source.rb
+++ iterations/refactored/cookbooks/nginx/recipes/source.rb
@@ -1,43 +1,32 @@
-cache_dir     = "/var/cache/downloads"
-tar_url       = "http://nginx.org/download/nginx-0.9.7.tar.gz"
-tar_checksum  = "2feb0acee473cc360a620ee862907b9570a4121956c40cbd27da35f5b0a96045"
+cache_dir     = node[:nginx][:archive_cache]
+tar_url       = node[:nginx][:tar_url]
+tar_checksum  = node[:nginx][:tar_checksum]
 tar_file      = tar_url.split('/').last
 tar_dir       = tar_file.sub(/\.tar\.gz$/, '')
 
-configure_flags = [
-  "--prefix=/opt/#{tar_dir}",
-  "--conf-path=/etc/nginx/nginx.conf",
-  "--with-http_ssl_module"
-]
+unless node[:nginx][:extra_configure_flags].empty?
+  node[:nginx][:configure_flags].push(*node[:nginx][:extra_configure_flags])
+end
+configure_flags = node[:nginx][:configure_flags]
 
-pkgs = %w{ wget build-essential binutils-doc autoconf flex bison
-           libpcre3 libpcre3-dev libssl-dev }
+pkgs = node[:nginx][:packages]
 
 pkgs.each do |pkg|
-  package pkg do
-    action :install
-  end
+  package pkg
 end
 
 directory cache_dir do
-  owner       "root"
-  group       "root"
   mode        "0755"
   recursive   true
-  action      :create
 end
 
 remote_file "#{cache_dir}/#{tar_file}" do
   source      tar_url
   checksum    tar_checksum
-  owner       "root"
-  group       "root"
   mode        "0644"
 end
 
 execute "extract nginx tarball" do
-  user      "root"
-  group     "root"
   cwd       cache_dir
   command   %{tar zxf #{tar_file}}
   creates   "#{cache_dir}/#{tar_dir}"
@@ -48,14 +37,58 @@ execute "compile nginx" do
   group     "root"
   cwd       "#{cache_dir}/#{tar_dir}"
   command   %{./configure #{configure_flags.join(' ')} && make && make install}
-  creates   "/opt/#{tar_dir}/sbin/nginx"
+  only_if do
+    any_missing = false
+    configure_flags.each do |flag|
+      result = %x{
+        if #{nginx_install}/sbin/nginx -V 2>&1 | grep -q -- "#{flag}" ; then
+          echo found
+        fi
+      }
+      any_missing = true unless result.chomp == "found"
+    end
+    if any_missing
+      true
+    else
+      creates node[:nginx][:src_binary]
+      false
+    end
+  end
 end
 
-cookbook_file "/etc/init.d/nginx" do
-  source  "nginx.init"
-  owner   "root"
-  group   "root"
-  mode    "0755"
+[ "/var/log/nginx", "/etc/nginx",
+  "/etc/nginx/conf.d", "/etc/nginx/sites-enabled"
+].each do |dir|
+  directory dir do
+    owner       "root"
+    group       "root"
+    mode        "0755"
+    recursive   true
+  end
+end
+
+template "/etc/nginx/nginx.conf" do
+  source    "nginx.conf.erb"
+  owner     "root"
+  group     "root"
+  mode      "0755"
+  notifies  :restart, "service[nginx]"
+end
+
+template "/etc/nginx/sites-enabled/_default.conf" do
+  source    "default-site.conf.erb"
+  owner     "root"
+  group     "root"
+  mode      "0755"
+  notifies  :restart, "service[nginx]"
+end
+
+template "/etc/init.d/nginx" do
+  source    "nginx.init"
+  owner     "root"
+  group     "root"
+  mode      "0755"
+  notifies  :restart, "service[nginx]"
 end
 
 service "nginx" do
